732e36346c08bbcbb1c532ce03d3989eb72b07aa
Erik Baalhuis <ebbaalhuis@gmail.com>
2015-10-06 15:18:16 +0200

    Van de BB site

---
+++ ds-prac2/prac2/Tasks.java
@@ -0,0 +1,40 @@
+package prac2;
+
+public class Tasks {
+
+  /* DATASTRUCTURES AND ALGORITHMS 2015-2016
+  * Programming assignment 2
+  * Deadline: Thursday October 8, 2015, before or at 23:59
+  * 
+  * Name(s) :
+  * VUid(s) :
+  */
+
+  // --------------------------------------------------------
+  //   METHODS FOR TASK 1 BELOW THIS LINE
+  // --------------------------------------------------------
+	
+  public static int task1 (int[] a) {
+  
+    return 0;
+  }
+  
+  // --------------------------------------------------------
+  //   METHODS FOR TASK 2 BELOW THIS LINE
+  // --------------------------------------------------------
+   
+  public static int task2 (int[] a) {
+  
+    return 0;
+  }
+  
+  // --------------------------------------------------------
+  //   METHODS FOR TASK 3 BELOW THIS LINE
+  // --------------------------------------------------------
+  
+  public static int task3 (int[][] m) { // m has size n x n (i.e. it is square)
+  
+    return 0;
+  }
+
+}
